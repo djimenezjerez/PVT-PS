d802050d3a88e9b753b4744a6ffa2355eb832a66
Alucarth <ltorrezs2008@gmail.com>
2018-08-13 14:55:58 -0400

    ultimos cambios

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -41,89 +41,93 @@ class Test extends Command
     {
         //
         global $rows,$rows_not_found;
+        
+        $this->info('testeando base prestamos');
+        $areas = DB::table('Area')->get();
+        $this->info(var_dump($areas));
 
-        $this->info("Ordenando No Conciliados XD");
-        $path = storage_path('no_conciliados.xlsx');
-        $this->info($path);
-        Excel::selectSheetsByIndex(0)->load($path , function($reader) {
+        // $this->info("Ordenando No Conciliados XD");
+        // $path = storage_path('no_conciliados.xlsx');
+        // $this->info($path);
+        // Excel::selectSheetsByIndex(0)->load($path , function($reader) {
             
-            // reader methods
-            global $rows,$rows_not_found;
+        //     // reader methods
+        //     global $rows,$rows_not_found;
             
-            $rows = array();
-            $rows_not_found = array();
-            array_push($rows, array('nro_prestamo','fecha_desembolso','producto','matricula', 'paterno', 'materno', 'primer_nombre','segundo_nombre', 'capital','interes','interes_penal','otros_cobros','total_pagado','tipo_descuento','nro_comprobante','*','ci','paterno','materno','primer_nombre','segundo_nombre','descuento'));
-            array_push($rows_not_found, array('nro_prestamo','fecha_desembolso','producto','matricula', 'paterno', 'materno', 'primer_nombre','segundo_nombre', 'capital','interes','interes_penal','otros_cobros','total_pagado','tipo_descuento','nro_comprobante','*','ci','paterno','materno','primer_nombre','segundo_nombre','descuento'));
-            // $rows = array();
+        //     $rows = array();
+        //     $rows_not_found = array();
+        //     array_push($rows, array('nro_prestamo','fecha_desembolso','producto','matricula', 'paterno', 'materno', 'primer_nombre','segundo_nombre', 'capital','interes','interes_penal','otros_cobros','total_pagado','tipo_descuento','nro_comprobante','*','ci','paterno','materno','primer_nombre','segundo_nombre','descuento'));
+        //     array_push($rows_not_found, array('nro_prestamo','fecha_desembolso','producto','matricula', 'paterno', 'materno', 'primer_nombre','segundo_nombre', 'capital','interes','interes_penal','otros_cobros','total_pagado','tipo_descuento','nro_comprobante','*','ci','paterno','materno','primer_nombre','segundo_nombre','descuento'));
+        //     // $rows = array();
 
-            $result = $reader->select(array('nro_prestamo','fecha_desembolso','producto','matricula', 'paterno', 'materno', 'primer_nombre','segundo_nombre', 'capital','interes','interes_penal','otros_cobros','total_pagado','tipo_descuento','nro_comprobante'))
-                           // ->take(100)
-                            ->get();
-            foreach($result as $row){
+        //     $result = $reader->select(array('nro_prestamo','fecha_desembolso','producto','matricula', 'paterno', 'materno', 'primer_nombre','segundo_nombre', 'capital','interes','interes_penal','otros_cobros','total_pagado','tipo_descuento','nro_comprobante'))
+        //                    // ->take(100)
+        //                     ->get();
+        //     foreach($result as $row){
                 
-                $arr= explode('-',$row->matricula);    
-                $ci= $arr[0];
-                $afiliado = DB::table('afiliados_comando')
-                                ->where('ci',$ci)
-                                //->where('tipo','=','')
-                                ->first();
-                if( isset($afiliado->id)){
+        //         $arr= explode('-',$row->matricula);    
+        //         $ci= $arr[0];
+        //         $afiliado = DB::table('afiliados_comando')
+        //                         ->where('ci',$ci)
+        //                         //->where('tipo','=','')
+        //                         ->first();
+        //         if( isset($afiliado->id)){
                     
-                    $total = $row->total_pagado;
+        //             $total = $row->total_pagado;
                     
-                        DB::table('afiliados_comando')
-                            ->where('id', $afiliado->id)
-                            ->update(['tipo' => $row->tipo_descuento.'_no_conciliado']);
-                        array_push($rows,array($row->nro_prestamo,$row->fecha_desembolso,$row->producto,$row->matricula,$row->paterno,$row->materno,$row->primer_nombre,$row->segundo_nombre,$row->capital,$row->interes,$row->interes_penal,$row->otros_cobros,$row->total_pagado,$row->tipo_descuento,$row->nro_comprobante,'*',$afiliado->ci,$afiliado->paterno,$afiliado->materno,$afiliado->primer_nombre,$afiliado->segundo_nombre,number_format($afiliado->descuento, 2, ',', '')));
-                        $this->info($row);
+        //                 DB::table('afiliados_comando')
+        //                     ->where('id', $afiliado->id)
+        //                     ->update(['tipo' => $row->tipo_descuento.'_no_conciliado']);
+        //                 array_push($rows,array($row->nro_prestamo,$row->fecha_desembolso,$row->producto,$row->matricula,$row->paterno,$row->materno,$row->primer_nombre,$row->segundo_nombre,$row->capital,$row->interes,$row->interes_penal,$row->otros_cobros,$row->total_pagado,$row->tipo_descuento,$row->nro_comprobante,'*',$afiliado->ci,$afiliado->paterno,$afiliado->materno,$afiliado->primer_nombre,$afiliado->segundo_nombre,number_format($afiliado->descuento, 2, ',', '')));
+        //                 $this->info($row);
                     
-                }
-                else{
-                    array_push($rows_not_found,array($row->nro_prestamo,$row->fecha_desembolso,$row->producto,$row->matricula,$row->paterno,$row->materno,$row->primer_nombre,$row->segundo_nombre,$row->capital,$row->interes,$row->interes_penal,$row->otros_cobros,$row->total_pagado,'*'));      
-                }
-                //$this->info($row);
+        //         }
+        //         else{
+        //             array_push($rows_not_found,array($row->nro_prestamo,$row->fecha_desembolso,$row->producto,$row->matricula,$row->paterno,$row->materno,$row->primer_nombre,$row->segundo_nombre,$row->capital,$row->interes,$row->interes_penal,$row->otros_cobros,$row->total_pagado,'*'));      
+        //         }
+        //         //$this->info($row);
                 
-            }
+        //     }
 
 
 
 
-        });
+        // });
 
-        Excel::create('no_conciliacion_junio',function($excel)
-        {
-            global $rows,$rows_not_found,$row_empy_capital;
-                    $excel->sheet('conciliados',function($sheet) {
-                            global $rows,$rows_not_found,$row_empy_capital;
-                            $sheet->fromModel($rows,null, 'A1', false, false);
-                            $sheet->cells('A1:C1', function($cells) {
-                            // manipulate the range of cells
-                            $cells->setBackground('#058A37');
-                            $cells->setFontColor('#ffffff');  
-                            $cells->setFontWeight('bold');
-                            });
-                        });
-                    $excel->sheet('no_conciliados',function($sheet) {
-                            global $rows,$rows_not_found,$row_empy_capital;
-                            $sheet->fromModel($rows_not_found,null, 'A1', false, false);
-                            $sheet->cells('A1:C1', function($cells) {
-                            // manipulate the range of cells
-                            $cells->setBackground('#058A37');
-                            $cells->setFontColor('#ffffff');  
-                            $cells->setFontWeight('bold');
-                            });
-                        });
-                    // $excel->sheet('no_captital_0',function($sheet) {
-                    //         global $rows,$rows_not_found,$row_empy_capital;
-                    //         $sheet->fromModel($row_empy_capital,null, 'A1', false, false);
-                    //         $sheet->cells('A1:C1', function($cells) {
-                    //         // manipulate the range of cells
-                    //         $cells->setBackground('#058A37');
-                    //         $cells->setFontColor('#ffffff');  
-                    //         $cells->setFontWeight('bold');
-                    //         });
-                    //     });
-        })->store('xls', storage_path());
-        $this->info('Finished');
+        // Excel::create('no_conciliacion_junio',function($excel)
+        // {
+        //     global $rows,$rows_not_found,$row_empy_capital;
+        //             $excel->sheet('conciliados',function($sheet) {
+        //                     global $rows,$rows_not_found,$row_empy_capital;
+        //                     $sheet->fromModel($rows,null, 'A1', false, false);
+        //                     $sheet->cells('A1:C1', function($cells) {
+        //                     // manipulate the range of cells
+        //                     $cells->setBackground('#058A37');
+        //                     $cells->setFontColor('#ffffff');  
+        //                     $cells->setFontWeight('bold');
+        //                     });
+        //                 });
+        //             $excel->sheet('no_conciliados',function($sheet) {
+        //                     global $rows,$rows_not_found,$row_empy_capital;
+        //                     $sheet->fromModel($rows_not_found,null, 'A1', false, false);
+        //                     $sheet->cells('A1:C1', function($cells) {
+        //                     // manipulate the range of cells
+        //                     $cells->setBackground('#058A37');
+        //                     $cells->setFontColor('#ffffff');  
+        //                     $cells->setFontWeight('bold');
+        //                     });
+        //                 });
+        //             // $excel->sheet('no_captital_0',function($sheet) {
+        //             //         global $rows,$rows_not_found,$row_empy_capital;
+        //             //         $sheet->fromModel($row_empy_capital,null, 'A1', false, false);
+        //             //         $sheet->cells('A1:C1', function($cells) {
+        //             //         // manipulate the range of cells
+        //             //         $cells->setBackground('#058A37');
+        //             //         $cells->setFontColor('#ffffff');  
+        //             //         $cells->setFontWeight('bold');
+        //             //         });
+        //             //     });
+        // })->store('xls', storage_path());
+        // $this->info('Finished');
     }
 }
